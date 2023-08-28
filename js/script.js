ee21c36901185260067c6955f03ce386eba61a4d
Ariel <arielalael@gmail.com>
2023-08-28 14:22:22 -0300

    refatoracao com class do scroll anima

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js'
-import initAnimacaoScroll from './modules/scroll-animacao.js'
+import ScrollAnima from './modules/scroll-anima.js'
 import Accordion from './modules/accordion.js'
 import TabNav from './modules/tabnav.js'
 import Modal from './modules/modal.js'
@@ -25,10 +25,12 @@ modal.init()
 const tooltip = new Tooltip('[data-tooltip]')
 tooltip.init()
 
-initAnimacaoScroll()
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]')
+scrollAnima.init()
+
 initDropdownMenu()
 initMenuMobile()
 initFuncionamento()
+
 fetchAnimais('../../animaisapi.json', '.numeros-grid')
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco')
-
