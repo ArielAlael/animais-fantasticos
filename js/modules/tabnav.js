2c5c14d5d678e58da5564da527996753ad35c012
Ariel <arielalael@gmail.com>
2023-09-19 09:41:10 -0300

    adicionado slide fotos

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,32 +1,32 @@
 export default class TabNav {
-    constructor (menu, content) {
-        this.tabMenu = document.querySelectorAll(menu)
-        this.tabContent = document.querySelectorAll(content)
-        this.activeClass = 'ativo'
-    }
-    
-    // Ativa a tab de acordo com o index da mesma
-    activeTab(index) {
-        this.tabContent.forEach((section) => {
-            section.classList.remove(this.activeClass)
-        })
-        const direcao = this.tabContent[index].dataset.anime
-        this.tabContent[index].classList.add(this.activeClass, direcao)
-    }
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = "ativo";
+  }
 
-    // Adiciona os eventos as tabs
-    addTabNavEvent() {
-        this.tabMenu.forEach((itemMenu, index) => {
-            itemMenu.addEventListener('click', () => this.activeTab(index))
-        })
-    }
+  // Ativa a tab de acordo com o index da mesma
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
+    });
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.activeClass, direcao);
+  }
 
-    init() {
-        if (this.tabMenu.length && this.tabContent.length) {
-            // Ativar primeiro item
-            this.activeTab(0)
-            this.addTabNavEvent()
-        }
-        return this
+  // Adiciona os eventos as tabs
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => this.activeTab(index));
+    });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      // Ativar primeiro item
+      this.activeTab(0);
+      this.addTabNavEvent();
     }
+    return this;
+  }
 }
